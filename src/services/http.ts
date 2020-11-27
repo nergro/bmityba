5492822a18608b8e85cf1ee1946dab551a15842e
Nerijus Gromas <nerijus.gromas@toughlex.com>
2020-11-27 19:17:16 +0200

    Change backend url

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
 
-export const baseUrl = 'https://bmityba.herokuapp.com/api';
+// export const baseUrl = 'https://bmityba.herokuapp.com/api';
+
+export const baseUrl = 'https://balanced-diet.herokuapp.com/api';
 axios.defaults.baseURL = baseUrl;
